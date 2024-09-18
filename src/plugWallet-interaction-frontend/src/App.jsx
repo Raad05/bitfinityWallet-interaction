7da0f089605452343515a7ab5c652103709f3cca
Raad05 <yamin.raad6109@gmail.com>
2024-09-18 15:49:33 +0600

    Initial commit

--- src/plugWallet-interaction-frontend/src/App.jsx
+++ src/plugWallet-interaction-frontend/src/App.jsx
@@ -1,31 +1,94 @@
-import { useState } from 'react';
-import { plugWallet_interaction_backend } from 'declarations/plugWallet-interaction-backend';
+import { useEffect, useMemo, useState } from "react";
 
-function App() {
-  const [greeting, setGreeting] = useState('');
+const App = () => {
+  const whitelist = useMemo(() => ["qoctq-giaaa-aaaaa-aaaea-cai"], []); // canister IDs
+  const [principal, setPrincipal] = useState("");
+  const [account, setAccount] = useState("");
+  const [isConnected, setIsConnected] = useState(false);
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    plugWallet_interaction_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
+  // connect to wallet if not connected
+  const connect = async () => {
+    const onConnectionUpdate = () => {
+      console.log(window.ic.plug.sessionManager.sessionData);
+    };
+
+    try {
+      const publicKey = await window.ic.plug.requestConnect({
+        whitelist,
+        onConnectionUpdate,
+        timeout: 50000,
+      });
+      alert("Wallet connected!");
+      console.log(`The connected user's public key is:`, publicKey);
+
+      const principalId = await window.ic.plug.principalId;
+      const accountId = await window.ic.plug.accountId;
+      setPrincipal(principalId);
+      setAccount(accountId);
+      setIsConnected(true);
+    } catch (e) {
+      alert("Failed to connect wallet.");
+      console.error(e);
+    }
+  };
+
+  // transfer ICP
+  const transfer = async () => {
+    alert("Transferred 0.1 ICP");
+  };
+
+  // get balance
+  const getBalance = async () => {
+    const balance = await window.ic.plug.requestBalance();
+
+    console.log(balance);
+  };
+
+  useEffect(() => {
+    // persist connection
+    const verifyConnection = async () => {
+      const connected = await window.ic.plug.isConnected();
+
+      if (connected) {
+        try {
+          const principalId = await window.ic.plug.principalId;
+          const accountId = await window.ic.plug.accountId;
+          setPrincipal(principalId);
+          setAccount(accountId);
+          setIsConnected(true);
+        } catch (e) {
+          console.error(e);
+        }
+      } else {
+        setIsConnected(false);
+      }
+    };
+
+    verifyConnection();
+  }, [whitelist]);
 
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
+    <div className="App">
+      <h1>PlugWallet Interaction</h1>
+      {isConnected ? (
+        <>
+          <p>
+            <strong>Principal ID:</strong> {principal}
+          </p>
+          <p>
+            <strong>Account ID:</strong> {account}
+          </p>
+          <button onClick={transfer}>Transfer 0.01 ICP</button>
+          <button onClick={getBalance}>Check Balance</button>
+        </>
+      ) : (
+        <>
+          <p>Plug is not connected.</p>
+          <button onClick={connect}>Connect to Plug</button>
+        </>
+      )}
+    </div>
   );
-}
+};
 
 export default App;
--- src/plugWallet-interaction-frontend/src/vite-env.d.ts
+++ src/plugWallet-interaction-frontend/src/vite-env.d.ts
@@ -1,1 +1,22 @@
 /// <reference types="vite/client" />
+export {};
+
+declare global {
+  interface Window {
+    ic: {
+      plug: {
+        sessionManager: {
+          sessionData;
+        };
+        requestConnect;
+        isConnected;
+        principalId;
+        accountId;
+        agent;
+        isConnected;
+        requestTransfer;
+        requestBalance;
+      };
+    };
+  }
+}
